c19c99910e04291552e89eda31d23b90bb384474
Bazyar <mbazyar@eav1050905m.ad.ea.com>
2020-03-11 03:51:29 -0700

    no longer needed

--- btg.sh
+++
@@ -1,3 +0,0 @@
-#bridge The gaps
-
-kubectl get rolebindings,clusterrolebindings   --all-namespaces    -o custom-columns='KIND:kind,NAMESPACE:metadata.namespace,NAME:metadata.name,SUBJECT:subjects[].name,Role:roleRef.name'
